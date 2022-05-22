044d8b1117ec41f197447391ca2659f2f4e9bc11
Your Name <you@example.com>
2022-05-22 14:54:46 +0600

    currection

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //Middle Ware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cepeo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri);
+
+const run = async()=>{
+    try{
+        await client.connect();
+        console.log('working');
+    }
+    finally{}
+}
+
+run().catch(console.dir)
+
+
 
 
 app.get('/', (req, res)=>{
