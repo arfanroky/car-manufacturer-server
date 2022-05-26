9ca5024d23051be997ae8c5c0c9dd8baf422ff9c
Your Name <you@example.com>
2022-05-26 17:09:40 +0600

    admin api is don

--- index.js
+++ index.js
@@ -17,53 +17,102 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
 // Verify Token
-const verifyToken = (req, res, next)=> {
+const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if(!authHeader){
-    return res.status(401).send({message: 'Unauthorized Access'})
+  if (!authHeader) {
+    return res.status(401).send({ message: 'Unauthorized Access' });
   }
 
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if(err){
-      return res.status(403).send({message: 'Forbidden Access'})
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden Access' });
     }
     req.decoded = decoded;
     next();
-    
-  })
-
-
-}
+  });
+};
 
 const run = async () => {
   try {
     await client.connect();
 
-    const carEquipmentsCollection = client.db('car_equipment').collection('equipment');
-    const orderCollection = client.db('car_equipment').collection('order');
-    const reviewCollection = client.db('car_equipment').collection('addReview');
+    const carEquipmentsCollection = client
+      .db('car_equipment')
+      .collection('equipment');
+    const ordersCollection = client.db('car_equipment').collection('order');
+    const reviewsCollection = client
+      .db('car_equipment')
+      .collection('addReview');
+    const usersCollection = client.db('car_equipment').collection('user');
 
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await usersCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === 'admin') {
+        next();
+      } else {
+        res.status(403).send({ message: 'forbidden' });
+      }
+    };
 
+    app.get('/user', verifyToken, async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    });
 
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user.role === 'admin';
+      res.send({ admin: isAdmin });
+    });
 
+    app.put('/user/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: 'admin' },
+      };
 
-      //Auth
-      app.post('/login', (req, res) => {
-        const email = req.body;
-        const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
-            expiresIn:'1d'
-        });
-        res.send({accessToken});
-    })
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
+    // Auth
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
 
-  // get all 
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: '1d',
+        }
+      );
+      res.send({ result, token: token });
+    });
+
+    // get all
     app.get('/equipment', async (req, res) => {
       const query = {};
-      const equipments = await (await carEquipmentsCollection.find(query).toArray()).reverse(-6);
+      const equipments = (
+        await carEquipmentsCollection.find(query).toArray()
+      ).reverse(-6);
       res.send(equipments);
     });
 
@@ -76,68 +125,58 @@ const run = async () => {
     });
 
     // review
-    app.post('/allReviews', async(req, res) => {
+    app.post('/allReviews', async (req, res) => {
       const ratings = req.body;
       console.log(ratings);
-      const addReviews = await reviewCollection.insertOne(ratings);
+      const addReviews = await reviewsCollection.insertOne(ratings);
 
-      res.send({success: true, addReviews})
+      res.send({ success: true, addReviews });
+    });
 
-    })
-
-    app.get('/allReviews', async(req, res) => {
+    app.get('/allReviews', async (req, res) => {
       const query = {};
-      const reviews = await (await reviewCollection.find(query).toArray()).reverse(-3);
+      const reviews = (await reviewsCollection.find(query).toArray()).reverse(
+        -3
+      );
 
-      res.send(reviews)
-    })
+      res.send(reviews);
+    });
 
-
-  
     //order post
     app.post('/order', async (req, res) => {
       const orderData = req.body;
-      const result = await orderCollection.insertOne(orderData);
+      const result = await ordersCollection.insertOne(orderData);
 
-      res.send({success:true, result});
-    })
+      res.send({ success: true, result });
+    });
 
     // order get
     app.get('/order', verifyToken, async (req, res) => {
       const decodedEmail = req.decoded.email;
       const email = req.query.email;
-      if(decodedEmail === email){
-        const query = {email: email};
-        const result = await orderCollection.find(query).toArray();
-        res.send(result)
-
+      if (decodedEmail === email) {
+        const query = { email: email };
+        const result = await ordersCollection.find(query).toArray();
+        res.send(result);
+      } else {
+        res.status(403).send('Forbidden Access');
       }
-      else{
-        res.status(403).send({message: 'forbidden access'})
-    }
-      
-    })
+    });
 
     app.delete('/order/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
-      const result = await orderCollection.deleteOne(query);
-      res.send({success:'Cancel Successfully', result})
-    })
-
-
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.send({ success: 'Cancel Successfully', result });
+    });
   } finally {
   }
 };
 
 run().catch(console.dir);
 
-// app.get('/', (req, res) => {
-//   res.send('Hello from Car Manufacturer');
-// });
-
 app.get('/', (req, res) => {
-  res.send('Hello from Car Manufacturer and try to solve heroku problem');
+  res.send('Hello from Car Manufacturer');
 });
 
 app.listen(port, () => {
