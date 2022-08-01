291a8e86c3412966f365a176cf442be2b241af10
Your Name <you@example.com>
2022-08-01 11:31:17 +0600

    created new api endpoint for reviews

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const stripe = require('stripe')(process.env.STRIPE_KEY)
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 //Middle Ware
 app.use(cors());
@@ -43,9 +43,7 @@ const run = async () => {
       .db('car_equipment')
       .collection('equipment');
     const ordersCollection = client.db('car_equipment').collection('order');
-    const reviewsCollection = client
-      .db('car_equipment')
-      .collection('addReview');
+    const reviewsCollection = client.db('car_equipment').collection('reviews');
     const usersCollection = client.db('car_equipment').collection('user');
     const paymentCollection = client.db('car_equipment').collection('payments');
 
@@ -66,51 +64,55 @@ const run = async () => {
       res.send(users);
     });
 
-    app.get('/user/:email', verifyToken, async(req, res) => {
+    app.get('/user/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
-      const query = {email: email};
+      const query = { email: email };
       const result = await usersCollection.findOne(query);
 
       res.send(result);
-    })
+    });
 
-  // UPDATE PROFILE
-  app.patch('/profileUpdate/:email', verifyToken, async (req, res) => {
-    const email = req.params.email;
-    const user = req.body;
-    const filter = { email: email };
-    const options = { upsert: true };
-    const updateDoc = {
-      $set: user
-    };
+    // UPDATE PROFILE
+    app.patch('/profileUpdate/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
 
-    const result = await usersCollection.updateOne(
-      filter,
-      updateDoc,
-      options
-    );
-      
-    res.send({success: true, result})
-  });
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
 
+      res.send({ success: true, result });
+    });
 
-    app.get('/admin/:email',verifyToken, async (req, res) => {
+    app.get('/admin/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
       const user = await usersCollection.findOne({ email: email });
       const isAdmin = user.role === 'admin';
       res.send(isAdmin);
     });
 
-    app.put('/user/admin/:email', verifyToken, verifyAdmin, async (req, res) => {
-      const email = req.params.email;
-      const filter = { email: email };
-      const updateDoc = {
-        $set: { role: 'admin' },
-      };
+    app.put(
+      '/user/admin/:email',
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
 
-      const result = await usersCollection.updateOne(filter, updateDoc);
-      res.send(result);
-    });
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+    );
 
     // Auth
     app.put('/user/:email', async (req, res) => {
@@ -128,7 +130,7 @@ const run = async () => {
         updateDoc,
         options
       );
-     
+
       const token = jwt.sign(
         { email: email },
         process.env.ACCESS_TOKEN_SECRET,
@@ -139,24 +141,17 @@ const run = async () => {
       res.send({ result, token: token });
     });
 
-
-
-    
-    app.post('/create-payment-intent', async(req, res) =>{
+    app.post('/create-payment-intent', async (req, res) => {
       const price = req.body.price;
       const amount = parseInt(price) * 100;
       const paymentIntent = await stripe.paymentIntents.create({
-        amount : amount,
+        amount: amount,
         currency: 'usd',
-        payment_method_types:['card']
+        payment_method_types: ['card'],
       });
 
-      res.send({clientSecret: paymentIntent.client_secret})
+      res.send({ clientSecret: paymentIntent.client_secret });
     });
-
-
-
-
 
     // get all
     app.get('/equipment', async (req, res) => {
@@ -167,13 +162,12 @@ const run = async () => {
       res.send(equipments);
     });
 
-    app.post('/equipment', async(req, res) => {
-     const equipment = req.body;
-     console.log(req?.body);
-    //  console.log(res);
-     const result = await carEquipmentsCollection.insertOne(equipment);
-     res.send(result)
-    })
+    app.post('/equipment', async (req, res) => {
+      const equipment = req.body;
+      console.log(req?.body);
+      const result = await carEquipmentsCollection.insertOne(equipment);
+      res.send(result);
+    });
 
     app.delete('/equipment/:id', async (req, res) => {
       const id = req.params.id;
@@ -182,19 +176,33 @@ const run = async () => {
       res.send({ success: 'Cancel Successfully', result });
     });
 
-    //Put 
-    app.put('/equipment/:id', async(req, res) => {
+    //Put
+    app.put('/equipment/:id', async (req, res) => {
       const id = req.params.id;
+      let avQuantity = '';
       const available_quantity = req.body.avQuantity;
-      const filter = {_id: ObjectId(id)};
-      const options = {upsert: true};
-      const doc = {
-        $set: {available_quantity}
-      }
-      const result = await carEquipmentsCollection.updateOne(filter, doc, options);
 
-      res.send({success: true, result})
-    })
+      if (available_quantity) {
+        avQuantity = available_quantity;
+      }
+
+      const review = req.body.review.rating;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const doc = {
+        $set: {
+          available_quantity: avQuantity,
+          review,
+        },
+      };
+      const result = await carEquipmentsCollection.updateOne(
+        filter,
+        doc,
+        options
+      );
+
+      res.send({ success: true, result });
+    });
 
     // get single
     app.get('/equipment/:id', async (req, res) => {
@@ -204,87 +212,94 @@ const run = async () => {
       res.send(result);
     });
 
-    // review
-    app.post('/allReviews',verifyToken, async (req, res) => {
-      const ratings = req.body;
-      const addReviews = await reviewsCollection.insertOne(ratings);
+    app.get('equipment/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await carEquipmentsCollection.findOne(query);
 
-      res.send({ success: true, addReviews });
+      res.send(result);
     });
 
-    // all Reviews
-    app.get('/allReviews', verifyToken, async (req, res) => {
+    //Review
+    app.put('/userReview/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const doc = { $set: req.body.review };
+      const result = await reviewsCollection.updateOne(filter, doc, options);
+      res.send({ success: 'Review added successfully', result });
+    });
+
+    app.get('/userReview', async(req, res) => {
       const query = {};
-      const reviews =  (await reviewsCollection.find(query).toArray()).reverse()
-      res.send({success: true, reviews});
-    });
+      const result = (await reviewsCollection.find(query).toArray()).reverse().slice(0, 3);
 
-
+      res.send(result);
+    })
 
     //order put
     app.put('/order/:id', async (req, res) => {
       const id = req.params.id;
       const orderData = req.body;
-      const options = {upsert: true};
-      const filter = {_id: ObjectId(id)};
+      const options = { upsert: true };
+      const filter = { _id: ObjectId(id) };
       const orderDoc = {
-        $set:orderData
-      }
-      const result = await ordersCollection.updateOne(filter, orderDoc, options);
+        $set: orderData,
+      };
+      const result = await ordersCollection.updateOne(
+        filter,
+        orderDoc,
+        options
+      );
 
-      res.send({success: true, result})
+      res.send({ success: true, result });
     });
 
     // order get
     app.get('/order', async (req, res) => {
       const query = {};
       const result = await ordersCollection.find(query).toArray();
-     
-      res.send(result)
+
+      res.send(result);
     });
 
-   
-    app.get('/order/payment', async(req, res) =>{
+    app.get('/order/payment', async (req, res) => {
       const id = req.query.id;
-      const filter = {_id: ObjectId(id)};
+      const filter = { _id: ObjectId(id) };
       const result = await ordersCollection.findOne(filter);
-      res.send({success: true, result});
-    })
-
+      res.send({ success: true, result });
+    });
 
     // order get by email
-    app.get('/order/:email',verifyToken, async (req, res) => {
+    app.get('/order/:email', verifyToken, async (req, res) => {
       const decodedEmail = req.decoded.email;
       const email = req.params.email;
-    
+
       if (decodedEmail === email) {
         const query = { email: email };
         const result = await ordersCollection.find(query).toArray();
         return res.send(result);
       } else {
-        return res.status(403).send({message: 'Forbidden Access'});
+        return res.status(403).send({ message: 'Forbidden Access' });
       }
     });
 
-
-  
-    app.patch('/order/:id',  async(req, res) =>{
+    app.patch('/order/:id', async (req, res) => {
       const id = req.params.id;
       const payment = req.body;
-      const filter = {_id: ObjectId(id)};
+      const filter = { _id: ObjectId(id) };
       const updateDoc = {
-        $set:{
+        $set: {
           paid: true,
-          paymentId: payment.transactionId
-        }
-      }
+          paymentId: payment.transactionId,
+        },
+      };
 
       const result = await paymentCollection.insertOne(payment);
       const updatedOrder = await ordersCollection.updateOne(filter, updateDoc);
 
-      res.send({success: true});
-    })
-
+      res.send({ success: true });
+    });
 
     app.delete('/order/:id', async (req, res) => {
       const id = req.params.id;
@@ -292,9 +307,6 @@ const run = async () => {
       const result = await ordersCollection.deleteOne(query);
       res.send({ success: 'Cancel Successfully', result });
     });
-
-
-
   } finally {
   }
 };
